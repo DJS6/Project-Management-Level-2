84cb56174ee57b502e2a1d0584ad3b91add47353
DJS6 <daniel.spaziani@student.onslow.school.nz>
2023-06-07 11:56:20 +1200

    Basic Planning of sprint 1. Planned battle function and started working on it.

---
+++ pokemon_onslow.py
@@ -0,0 +1,23 @@
+def battle(your_name, your_hp, your_type, enemy_name, enemy_hp, enemy_type):
+    """Starts the combat loop, and returns relevant info at the end."""
+    # Repeats until combat finished.
+    while True:
+        # Display information (and graphic?) to user.
+        # Displays enemy's name, as well as their health.
+        # And the type of pokemon they are.
+        print("""
+ENEMY POKÉMON:                                      YOUR POKÉMON:
+Name: {}                                            Name: {}
+HP: {}                                              HP: {}
+{} Type                                             {} Type""".format(
+            enemy_name, your_name, enemy_hp, your_hp, enemy_type, your_type))
+        # Let them make choice around which attack to use.
+        # Calculate damage.
+        # Inform user of updates.
+        # Check if defeated, if so exit.
+        # Switch to enemy.
+        # Choose attack for enemy.
+        # Calculate damage.
+        # Inform user of updates.
+        # Check if defeated, if so exit.
+        # Switch to user.
