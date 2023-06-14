77c0daae1ec591cad3969560e9e8e19accc813f6
DJS6 <daniel.spaziani@student.onslow.school.nz>
2023-06-15 11:42:01 +1200

    Fixed some messages and made damage calc template Fixed bad messages and changed damage ranges for effectiveness messages.

--- pokemon_onslow.py
+++ pokemon_onslow.py
@@ -44,6 +44,18 @@ def damage_calculator(attacker_attack_choice, type_of_attacked) -> int:
     """Uses attack chosen and type of pokemon attacked to calculate damage,
     Based on which attacks are which effective against which types."""
     # TODO: Function.
+    # Example Structure, can copy and fill names after feedback.
+    # Gets the type of pokemon that is being attacked.
+    if type_of_attacked == "Type1":
+        # If attack is effective against this type, it can deal 200-300 damage.
+        if attacker_attack_choice == "Type1_effective_attack":
+            return randint(200, 300)
+        # If it is only moderately effective, it deals between 100-200.
+        elif attacker_attack_choice == "Type1_moderately_effective_attack":
+            return randint(100, 200)
+        # If it is not effective it can deal no more than 100.
+        elif attacker_attack_choice == "Type1_not_effective_attack":
+            return randint(1, 100)
 
 
 def battle(your_name: str, your_hp: int, your_type: str, your_attacks: list,
@@ -75,13 +87,19 @@ HP: {}                                              HP: {}
         # Inform user of updates.
         # Every pokemon has 1000 hp total.
         # Message saying how effective it was based on damage ranges.
+        # If between 0 and 50 then it's not effective.
         if your_attack_damage < 50:
-            print("Your attack was ineffective!")
+            print("{}'s attack was ineffective!".format(your_name))
+        # 50-150 is effective, but not very.
         elif your_attack_damage < 150:
-            print("Your attack was quite effective!")
+            print("{}'s attack was somewhat effective!".format(your_name))
+        # 150-215 is more effective, but it can still be better.
         elif your_attack_damage < 215:
-            print("Your attack was VERY EFFECTIVE!")
-        print("{} lost {} HP!".format(enemy_name))
+            print("{}'s attack was effective!".format(your_name))
+        # 215 and upwards (to max of 300) is the most effective kind.
+        else:
+            print("{}'s attack was VERY EFFECTIVE!".format(your_name))
+        print("{} lost {} HP!".format(enemy_name, your_attack_damage))
         # Check if defeated, if so exit.
         if enemy_hp <= 0:
             print("{} is defeated!".format(enemy_name))
@@ -95,13 +113,19 @@ HP: {}                                              HP: {}
         enemy_attack_damage = damage_calculator(enemy_attack_choice, your_type)
         # Inform user of updates.
         # Message saying how effective it was based on damage ranges.
-        if your_attack_damage < 50:
-            print("Your attack was ineffective!")
-        elif your_attack_damage < 150:
-            print("Your attack was quite effective!")
-        elif your_attack_damage < 215:
-            print("Your attack was VERY EFFECTIVE!")
-        print("{} lost {} HP!".format(your_name))
+        # If between 0 and 50 then it's not effective.
+        if enemy_attack_damage < 50:
+            print("{}'s attack was ineffective!".format(enemy_name))
+        # 50-150 is effective, but not very.
+        elif enemy_attack_damage < 150:
+            print("{}'s attack was somewhat effective!".format(enemy_name))
+        # 150-215 is more effective, but it can still be better.
+        elif enemy_attack_damage < 215:
+            print("{}'s attack was effective!".format(enemy_name))
+        # 215 and upwards (to max of 300) is the most effective kind.
+        else:
+            print("{}'s attack was VERY EFFECTIVE!".format(enemy_name))
+        print("{} lost {} HP!".format(your_name, enemy_attack_damage))
         # Check if defeated, if so exit.
         if your_hp <= 0:
             print("{} is defeated!".format(your_name))
